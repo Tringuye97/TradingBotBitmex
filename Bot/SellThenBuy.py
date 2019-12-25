43173aec358e70d0b44896f8cd1b13d99df88272
Tri Nguyexxx <vantringuyen97@gmail.com>
2019-12-24 22:25:46 -0500

    Update 12/24/2019

--- Bot/SellThenBuy.py
+++ Bot/SellThenBuy.py
@@ -21,45 +21,45 @@ def run():
     while(ws.ws.sock.connected):
 
         print('\n')
-        # logger.info("Ticker: %s" % ws.get_tickerXBTUSD())
+        logger.info("Ticker: %s" % ws.get_tickerXBTUSD())
         # logger.info("Positions: %s" % ws.positions())
-        currentState = ws.get_tickerXBTUSD()
+        # currentState = ws.get_tickerXBTUSD()
         
         a = ws.open_orders()
-        print(len(a)) 
-        ##################### Ideal condition to SELL and BUY
-        # Place Selling Order if there is no position and no active order
-        if currentState['Ask Size']<1380000 and currentState['Bid Size']<1380000 and ws.positions()['Quantity']==0 and len(a)==0:
-            PositionPrice = ws.get_tickerXBTUSD()['Ask Price']
-            # ContractQty = -int(ws.funds()['Wallet Balance']*ws.get_tickerXBTUSD()['Ask Price'])    # A negative number
-            place_order(Quantity=ContractQty, Price=PositionPrice, api_key=api_key, api_secret=api_secret)
+        print(a) 
+        # ##################### Ideal condition to SELL and BUY
+        # # Place Selling Order if there is no position and no active order
+        # if currentState['Bid Size']<1380000 and ws.positions()['Quantity']==0 and len(a)==0:
+        #     PositionPrice = ws.get_tickerXBTUSD()['Ask Price']
+        #     # ContractQty = -int(ws.funds()['Wallet Balance']*ws.get_tickerXBTUSD()['Ask Price'])    # A negative number
+        #     place_order(Quantity=ContractQty, Price=PositionPrice, api_key=api_key, api_secret=api_secret)
         
-        # sleep(11)
-        # Place Buy Order if there is a position and no active order
-        # a = ws.open_orders()
-        # print(len(a)) 
-        logger.info("Positions: %s" % ws.positions())
-        print(ContractQty)
-        print(ws.positions()['Quantity'])
-        if ws.positions()['Quantity']==ContractQty and len(a)==0:
-            print('Placing Buy Order\n')
-            CurrentBidPrice = ws.get_tickerXBTUSD()['Bid Price']      # Get current Bid Price
-            if (CurrentBidPrice>=PositionPrice-0.5 and CurrentBidPrice<PositionPrice+168):     # PositionPrice-0.5<=CurrentBidPrice<PositionPrice+168 
-                place_order(Quantity=-ContractQty, Price=PositionPrice-0.5, api_key=api_key, api_secret=api_secret)
-            else:   # the rest of the cases
-                place_order(Quantity=-ContractQty, Price=PositionPrice, api_key=api_key, api_secret=api_secret)
+        # # sleep(11)
+        # # Place Buy Order if there is a position and no active order
+        # # a = ws.open_orders()
+        # # print(len(a)) 
+        # logger.info("Positions: %s" % ws.positions())
+        # print(ContractQty)
+        # print(ws.positions()['Quantity'])
+        # if ws.positions()['Quantity']==ContractQty and len(a)==0:
+        #     print('Placing Buy Order\n')
+        #     CurrentBidPrice = ws.get_tickerXBTUSD()['Bid Price']      # Get current Bid Price
+        #     if (CurrentBidPrice>=PositionPrice-0.5 and CurrentBidPrice<PositionPrice+168):     # PositionPrice-0.5<=CurrentBidPrice<PositionPrice+168 
+        #         place_order(Quantity=-ContractQty, Price=PositionPrice-0.5, api_key=api_key, api_secret=api_secret)
+        #     else:   # the rest of the cases
+        #         place_order(Quantity=-ContractQty, Price=PositionPrice, api_key=api_key, api_secret=api_secret)
 
-        ##################### Price decreased more than 11 units from ACTIVE sell order price
-        if (len(a)!=0):
-            CurrentAskPrice = ws.get_tickerXBTUSD()['Ask Price']
-            if a[0]['Side']=='Sell' and a[0]['Price']>=(CurrentAskPrice+11):
-                print("Cancel All Orders\n")
-                cancel_order(api_key=api_key, api_secret=api_secret)
-                PositionPrice = ws.get_tickerXBTUSD()['Ask Price']
-                # ContractQty = -int(ws.funds()['Wallet Balance']*ws.get_tickerXBTUSD()['Ask Price'])    # A negative number
-                place_order(Quantity=ContractQty, Price=PositionPrice, api_key=api_key, api_secret=api_secret)
+        # ##################### Price decreased more than 11 units from ACTIVE sell order price
+        # if (len(a)!=0):
+        #     CurrentAskPrice = ws.get_tickerXBTUSD()['Ask Price']
+        #     if a[0]['Side']=='Sell' and a[0]['Price']>=(CurrentAskPrice+11):
+        #         print("Cancel All Orders\n")
+        #         cancel_order(api_key=api_key, api_secret=api_secret)
+        #         PositionPrice = ws.get_tickerXBTUSD()['Ask Price']
+        #         # ContractQty = -int(ws.funds()['Wallet Balance']*ws.get_tickerXBTUSD()['Ask Price'])    # A negative number
+        #         place_order(Quantity=ContractQty, Price=PositionPrice, api_key=api_key, api_secret=api_secret)
 
-        sleep(11)
+        sleep(13)
      
         
             
